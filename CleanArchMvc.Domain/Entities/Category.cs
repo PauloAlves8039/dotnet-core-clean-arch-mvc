3e49cde41cc030343df3efd646bf78ef2f673168
PauloAlbes8039 <pj38alves@gmail.com>
2021-06-12 17:47:11 -0300

    Implementando as entidades do modelo de domínio

---
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,40 @@
+﻿using CleanArchMvc.Domain.Validation;
+using System.Collections.Generic;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    public sealed class Category : Entity
+    {
+        public string Name { get; private set; }
+
+        public ICollection<Product> Products { get; set; }
+
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id value");
+            Id = id;
+            ValidateDomain(name);
+        }
+
+        public void Update(string name) 
+        {
+            ValidateDomain(name);
+        }
+
+        private void ValidateDomain(string name) 
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), 
+                "Invalid name. Name is required");
+
+            DomainExceptionValidation.When(name.Length < 3,
+                "Invalid name, too short, minimum 3 characters");
+
+            Name = name;
+        }        
+    }
+}
---
+++ CleanArchMvc.Domain/Entities/Entity.cs
@@ -0,0 +1,7 @@
+﻿namespace CleanArchMvc.Domain.Entities
+{
+    public abstract class Entity
+    {
+        public int Id { get; protected set; }
+    }
+}
