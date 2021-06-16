9d725b05ddb83d02c4ffc572f3fd5015b31c4636
PauloAlbes8039 <pj38alves@gmail.com>
2021-06-16 14:18:59 -0300

    Registrando os SeverityCodeDescriptionProjectFileLineSuppression State

--- CleanArchMvc.IoC/DependencyInjection.cs
+++ CleanArchMvc.IoC/DependencyInjection.cs
@@ -1,4 +1,7 @@
-﻿using CleanArchMvc.Domain.Interfaces;
+﻿using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Application.Mappings;
+using CleanArchMvc.Application.Services;
+using CleanArchMvc.Domain.Interfaces;
 using CleanArchMvc.Infra.Data.Context;
 using CleanArchMvc.Infra.Data.Repositories;
 using Microsoft.EntityFrameworkCore;
@@ -17,6 +20,9 @@ namespace CleanArchMvc.IoC
 
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
 
             return services;
         }
