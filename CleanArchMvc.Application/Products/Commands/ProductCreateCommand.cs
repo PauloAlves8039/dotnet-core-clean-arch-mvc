776b2244af72bb69efab9fc23fcea26828381151
PauloAlbes8039 <pj38alves@gmail.com>
2021-06-17 11:04:27 -0300

    Usando CQRS para implementar Commands e Queries em Product

---
+++ CleanArchMvc.Application/Products/Commands/ProductCreateCommand.cs
@@ -0,0 +1,6 @@
+﻿namespace CleanArchMvc.Application.Products.Commands
+{
+    public class ProductCreateCommand : ProductCommand
+    {
+    }
+}
---
+++ CleanArchMvc.Application/Products/Queries/GetProductsQuery.cs
@@ -0,0 +1,10 @@
+﻿using CleanArchMvc.Domain.Entities;
+using MediatR;
+using System.Collections.Generic;
+
+namespace CleanArchMvc.Application.Products.Queries
+{
+    public class GetProductsQuery : IRequest<IEnumerable<Product>>
+    {
+    }
+}
