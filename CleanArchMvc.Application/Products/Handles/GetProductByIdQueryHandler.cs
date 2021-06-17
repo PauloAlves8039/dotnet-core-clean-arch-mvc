c192ecafb7a24a2819f33719a5698ecffe0b7fde
PauloAlbes8039 <pj38alves@gmail.com>
2021-06-17 14:39:45 -0300

    Usando CQRS para implementar os Handlers de Product

---
+++ CleanArchMvc.Application/Products/Handles/GetProductByIdQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using CleanArchMvc.Application.Products.Queries;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Application.Products.Handles
+{
+    public class GetProductByIdQueryHandler : IRequestHandler<GetProductByIdQuery, Product>
+    {
+        private readonly IProductRepository _productRepository;
+
+        public GetProductByIdQueryHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
+        }
+        
+        public async Task<Product> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+        {
+            return await _productRepository.GetByIdAsync(request.Id);
+        }
+    }
+}
---
+++ CleanArchMvc.Application/Products/Handles/ProductRemoveCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using CleanArchMvc.Application.Products.Commands;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Application.Products.Handles
+{
+    public class ProductRemoveCommandHandler : IRequestHandler<ProductRemoveCommand, Product>
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductRemoveCommandHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
+        }
+
+        public async Task<Product> Handle(ProductRemoveCommand request, CancellationToken cancellationToken)
+        {
+            var product = await _productRepository.GetByIdAsync(request.Id);
+
+            if (product == null)
+            {
+                throw new ApplicationException($"Entity could not be found.");
+            }
+            else 
+            {
+                var result = await _productRepository.RemoveAsync(product);
+                return result;
+            }
+        }
+    }
+}
